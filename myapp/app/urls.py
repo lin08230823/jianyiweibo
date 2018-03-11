dbba973d25749d2c3b16839546d68aa050ffd147
Xu <1035499675@qq.com>
2018-03-11 22:10:12 +0800

    4.1

--- myapp/app/urls.py
+++ myapp/app/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from .views import homepage
+from .views import homepage,user_page
 app_name = 'weibo'
 urlpatterns = [
     path('', homepage, name='homepage'),
-
+    path('u',user_page, name='upage')
 ]
