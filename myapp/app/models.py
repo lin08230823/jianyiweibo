7404280d7c31072d8b4b787e997bfa49c82ec89c
Xu <1035499675@qq.com>
2018-03-14 21:42:01 +0800

    9.0

--- myapp/app/models.py
+++ myapp/app/models.py
@@ -28,7 +28,9 @@ class WBUser(User):
     def save_user_info(self, info: dict):
         self._info = json.dumps(info)
         self.save()
-    def update(self, msg):
+
+
+    def update(self, msg: str):
         wbt = WBText.objects.create(author=self, msg=msg )
         wb = WeiBo.objects.create(user=self, text=wbt)
         return wb
@@ -40,12 +42,16 @@ class WBUser(User):
 
     def forward(self, weibo: 'WeiBo'):
         return WeiBo.objects.create(user=self, weibo_text=weibo.weibo_text)
+
+
     def __str__(self):
         return self.name
 
 class WBText(models.Model):
-    author = models.OneToOneField(WBUser, verbose_name='作者',on_delete=models.CASCADE)
+    author = models.ForeignKey(WBUser, verbose_name='作者',on_delete=models.CASCADE)
     msg = models.TextField(verbose_name='微博', max_length=500)
+    def __str__(self):
+        return '{msg}...'.format(msg=self.msg[:20])
 
 class WeiBo(models.Model):
     user = models.ForeignKey(WBUser,verbose_name='用户', related_name='weibos',on_delete=models.CASCADE)
@@ -56,15 +62,23 @@ class WeiBo(models.Model):
     def del_this(self):
         self.is_del = True
         self.save()
+    def comment_this(self, user:WBUser,text:str):
+        comment = Comment.objects.create(user=user, text=text, target=self)
+        return comment
+    def __str__(self):
+        return self.text.__str__()
 
 
 class Comment(models.Model):
-    target = models.ForeignKey(WBText,verbose_name='被评信息',related_name='comments',on_delete=models.CASCADE)
+    target = models.ForeignKey(WeiBo,verbose_name='微博',related_name='comments',on_delete=models.CASCADE)
     user = models.ForeignKey(WBUser,verbose_name='用户', related_name='comments', on_delete=models.CASCADE)
-    text = models.OneToOneField(WBText, verbose_name='评论内容',on_delete=models.CASCADE)
+    text = models.TextField(verbose_name='评论内容', max_length=500)
     time_create = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
     is_del = models.BooleanField(verbose_name='是否删除',default=False)
 
     def del_this(self):
         self.is_del = True
         self.save()
+
+    def __str__(self):
+        return'{msg}...'.format(msg=self.text[:20])
--- myapp/app/views.py
+++ myapp/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import WBUser
+from .models import WBUser,WeiBo
 # Create your views here.
 
 
@@ -17,8 +17,10 @@ def homepage(request):
 def user_page(request):
     uid = request.GET.get('uid')
     wb_user = get_object_or_404(WBUser, id=uid)
+    wbs = WeiBo.objects.filter(user=wb_user).order_by("-time_create")
     return render(request,'weibo/user_page.html',{
-        'wb_user': wb_user
+        'wb_user': wb_user,
+        'wbs': wbs
     })
 
 def wb_update(request):
